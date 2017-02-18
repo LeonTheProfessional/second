496160c3214f636e9c060afbd45557f7c2142d36
LeonTheProfessional <hasee@ARRIETTY-UNDER-FLOOR>
2017-02-18 23:49:44 +0800

    better

--- 源.c
+++ 源.c
@@ -2,31 +2,32 @@
 #include<stdio.h>
 #include<string.h>
 #define limmite 5
-char*sort(char*);
+#define litter 80
+void sort(char*[]);
 int main(void)
 {
-	char *ptr[limmite];
+	char ptr[limmite][litter];
 	char *after[limmite];
 	int count;
-	puts("Please enter some sentence.I will help you sort them.\n");
+	puts("Please enter 5 sentence.I will help you sort them.");
 	for (count = 0; count < limmite; count++)
 		gets(ptr[count]);
-	after[0] = sort(ptr);
+	after[0] = ptr[0];
+	sort(after);
 	while (count--)
 		puts(after[count]);
 	return 0;
 }
-char*sort(char*ptr)
+void sort(char*after[])
 {
 	char*temp;
-	int top,seat,count;
-	for(seat =0; seat<limmite-1; seat++)
-		for(count=1,top=0;count<=limmite;count++)
-			if (strcmp(ptr[count], ptr[top]))
+	int top, seek;
+	for(top =0; top<limmite-1; top++)
+		for(seek=top+1;seek<=limmite;seek++)
+			if (strcmp(after[seek], after[top]))
 			{
-				temp = ptr[count];
-				ptr[top] = ptr[count];
-				ptr[count] = temp;
+				temp = after[seek];
+				after[top] = after[seek];
+				after[seek] = temp;
 			}
-	return ptr;
 }
